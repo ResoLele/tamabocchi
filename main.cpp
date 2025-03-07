79cac34dfcc080268cc5088d18b8ca3ae14b7ada
YUKI.N <cp862005@gmail.com>
2025-03-07 23:15:51 +0800

    2025-03-07

--- main.cpp
+++ main.cpp
@@ -1,19 +1,39 @@
 #include <iostream>
 #include <filesystem>
 #include <string>
-#include "tama_dir.cpp"
-#include "tama_usr.cpp"
+#include "tama_file.cpp"
 
 using namespace std;
 
 // variable declaration
 int userInputAction;
 
-int main()
-{
-	while (true) {
-		cout << "Enter Action: ";
-		cin >> userInputAction;
-		userAction(userInputAction);
-	}		
+enum Action {   ACTION_CHANGE_DIR = 1,
+                ACTION_LIST_DIR,
+                ACTION_END_TASK,
+            };
+
+int main() {
+
+    // Initialize
+    fileInit();
+
+    do {
+        cout << "Enter Action: ";
+    	cin >> userInputAction;
+
+        switch (userInputAction) {
+            case ACTION_CHANGE_DIR:
+                fileChangeDir();
+                break;
+            case ACTION_LIST_DIR:
+                fileListDir();
+                break;
+            default:
+                cerr << "Invaild Action." << endl;
+                break;
+        }
+    } while (userInputAction != ACTION_END_TASK);
+    
+    return 0;
 }
---
+++ tama_file.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <filesystem>
+#include <fstream>
+
+using namespace std;
+namespace fs = std::filesystem;
+
+// VARIABLES
+fs::path USER_PATH;
+string DEFAULT_PATH;
+
+void fileInit () {
+	USER_PATH = fs::current_path();
+}
+
+// FUNCTIONS
+void fileChangeDir () {
+	do {
+		cin >> USER_PATH;
+		if (fs::directory_entry{USER_PATH}.exists()) {
+			cout << "Directory changed" << '\n';
+			cout << "Current Directory: " << USER_PATH << endl;
+		} else {
+			cout << "Directory not found!" << endl;
+		}
+	} while (!fs::directory_entry{USER_PATH}.exists());
+}
+
+void fileListDir () {
+	for (const auto& dirEntry : fs::directory_iterator(USER_PATH)) {
+		cout << dirEntry.path() << '\n'; 
+	}
+}
